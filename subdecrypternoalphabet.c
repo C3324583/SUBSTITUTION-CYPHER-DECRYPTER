7cf07c1bcd757beb47e7bf231bf5d5a9e1975480
William Millard Jorda <c3324583@uon.edu.au>
2019-04-26 02:06:15 +0000

    kugcyf

--- subdecrypternoalphabet.c
+++ subdecrypternoalphabet.c
@@ -4,9 +4,9 @@
 
 int main(){
     //initialisation of variables and arrays
-	char text[1000];
+	char text[10000];
 	int number_of_letter[127] = {0};
-	int n, i, x, ascii, rotationey, MAX = 0;
+	int n, i, x, y, ascii, rotationkey, MAX = 0, common_letter;
 	
 	printf("Enter message: ");//prompts the user to imput a message
 	scanf(" %[^\n]s", text);//stores the text imputted by the user in the array "text"
@@ -14,16 +14,10 @@ int main(){
    /*This for loop executes for all characters in the array "text" until the chracter occupying index i is a
     "NULL" character*/
     for(n=0; text[n]!='\0'; n++){
-  
 	    //turns lowercase letters in the array "text" into UPPERCASE by subtracting 32
      	if(text[n] >= 'a' && text[n] <= 'z'){
 		    text[n] = text[n] - 32;
      	}
-     
-        //prints the character at index "n" to the screen if it is not a letter
-        if(text[n] < 'A' || text[n] > 'Z'){
-                printf("%c", text[n]);
-        }
     }
     
 	for(ascii='A'; ascii<='Z'; ascii++){
@@ -34,14 +28,25 @@ int main(){
         } 
 	}
 	
-	for (x=0; number_of_letter[x] != '\0'; x++){
+	for (x=0; x<126; x++){
 	    if(number_of_letter[x] > MAX){
 	        MAX = number_of_letter[x];
 	        common_letter = x;
 	    }
 	}
-	
+
 	rotationkey = common_letter - 'E';
 	
+    for(y = 0; text[y] != '\0'; y++){
+        if(text[y] >= 'A' && text[y] <= 'Z'){
+    		text[y] = text[y] - rotationkey;
+    		if(text[y] > 'Z' || text[y] < 'A'){
+				text[y] = text[y] + 26;
+			}
+	    }	
+    }
+
+	printf("The decrypted message is: %s", text); //prints the rotated message to the screen	
+		
 	return 0;
 }
